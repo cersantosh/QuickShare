99a3ae793ee308e1e86a35ce8bbae334e64ad9a2
uniqueboy1 <information11993@gmail.com>
2023-11-18 13:29:04 +0545

    fjdjf

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
   app.get("*", (req, res) => {
     res.sendFile("frontend/build/index.html");
-    // res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 
   });
 } else {
