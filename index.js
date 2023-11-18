8bac8c5c4ed50181bfe4d4f0fab0e86bbdeca5fd
uniqueboy1 <information11993@gmail.com>
2023-11-18 14:11:07 +0545

    fdfj

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ import { Server } from "socket.io";
 import messages from "./models/messages.js";
 import firebaseStorage from "./utils/firebase_initialize.js";
 import { ref, deleteObject } from "firebase/storage";
-import path from "path";
-
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 const app = express();
 app.use(cors());
@@ -22,8 +22,9 @@ app.use("/messages", messageRouter);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,"frontend/build/index.html"));
-
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    res.sendFile(path.resolve(__dirname, "frontend/build/index.html"));
   });
 } else {
   app.get("/", (req, res) => {
