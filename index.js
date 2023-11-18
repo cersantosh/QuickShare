8020ec5315386926932601c5f09df5bc398dc70c
uniqueboy1 <information11993@gmail.com>
2023-11-18 13:25:50 +0545

    fjdjf

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ app.use("/messages", messageRouter);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
   app.get("*", (req, res) => {
-    // res.sendFile("frontend/build/index.html");
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile("frontend/build/index.html");
+    // res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 
   });
 } else {
